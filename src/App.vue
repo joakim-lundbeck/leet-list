83ae9448dc1ee65c4a80240abbd5b3678afe3e53
joakim-lundbeck <joakim.lundbeck@gmail.com>
2021-05-23 13:24:43 +0200

    Axios and get request
    Installed Axios and query the api for list of employees

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,29 @@
 export default {
   name: 'Leet-List',
   
+  data () {
+    return {
+      enabledEmployees: []  
+    }
+  },
+
+  mounted () {
+    const axios = require('axios')
+
+    axios
+      .get('https://api.1337co.de/v3/employees', {
+        headers: {
+          'Authorization': 'api-key 14:2021-05-18:lucas.stenberg@tretton37.com 5022ab727cb0cb271ff3a7f59b15cedd7c4c63a47c833668d4dde6adedc557f0'
+        }
+      }
+    )
+    .then(
+      response => {
+        this.enabledEmployees = response.data.filter(e => e.published)
+        console.table(this.enabledEmployees)
+      }
+    )
+  }
+
 }
 </script>
