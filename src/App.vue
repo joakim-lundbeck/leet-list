09f9b06bb2192bf868ecbe7fe7a4d1eebf7a1a45
joakim-lundbeck <joakim.lundbeck@gmail.com>
2021-05-23 13:41:07 +0200

    Added EmployeeCard component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app" class="container">
     <h1>The fellowship of the Tretton37</h1>
+
+    <div class="row">
+      <div v-for="employee in enabledEmployees" :key="employee.email" class="col-3">
+        <EmployeeCard :employee="employee"></EmployeeCard>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import EmployeeCard from './components/EmployeeCard.vue'
 
 export default {
   name: 'Leet-List',
   
+  components: {
+    EmployeeCard
+  },
+
   data () {
     return {
       enabledEmployees: []  
---
+++ src/components/EmployeeCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card text-white bg-dark mb-3 me-2">
+    
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12">
+                <h5 class="card-title">{{employee.name}}</h5>
+                </div>
+            </div>
+                            
+            <p class="card-text">
+                <i class="bi bi-building"></i>
+                {{employee.office}}
+            </p>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'EmployeeCard',
+  props: [ 'employee' ]
+}
+</script>
