e2471977c24822854e91347d0ba4a40d55c4470a
joakim-lundbeck <joakim.lundbeck@gmail.com>
2021-05-23 14:14:45 +0200

    Added Filters component
    Added component for filters, adding name input and using computed property filteredEmployees instead of enabledEmployees when rendering EmployeeCard component

---
+++ src/components/Filters.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card-body">
+        <div class="input-group">
+
+            <input 
+                type="text" 
+                class="form-control" 
+                placeholder="Name Search" 
+                v-model="filter"
+                @keyup="$emit('update:nameFilter', filter)" >
+
+            <button
+                v-if="filter.length > 0" 
+                class="btn btn-outline-secondary" 
+                type="button"
+                @click="$emit('update:nameFilter', ''), filter=''">
+
+            <i class="bi bi-x-circle"></i>
+
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: [ 'nameFilter' ],
+
+    data() {
+        return {
+        filter: ''
+        }
+    },
+}
+
+</script>
